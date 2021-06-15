48b948874ef955f8885846321ac6b78999f853c7
Megha Mehta <mmehta.ten@gmail.com>
2021-06-15 23:34:19 +0530

    delete terraform.tf

--- terraform.tf
+++
@@ -1,8 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket  = "terraform-backend-in-s3-for-eks-with-terraform"
-#     encrypt = true
-#     key     = "terraform.tfstate"
-#     region  = "us-east-1"
-#   }
-# }
