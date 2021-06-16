34455ebae5bb9b5658c93f795e1e0ac3b40db265
Megha Mehta <mmehta.ten@gmail.com>
2021-06-16 16:09:25 +0530

    update shell script to check ing

--- create-eks-deploy-app.sh
+++ create-eks-deploy-app.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+terraform init
+
 # Create terraform backend in AWS S3
 terraform -chdir=modules/terraform-backend-s3 apply -auto-approve
 
 # Create VPC, subnets, EKS cluster and ingress-controller
 terraform apply -auto-approve
-terraform output eks_kubeconfig > .kubeconfig
+terraform output eks_kubeconfig > /tmp/.kubeconfig
+
+sed -n -e "2,$(($(wc -l < /tmp/.kubeconfig) - 1))p" /tmp/.kubeconfig > .kubeconfig
 
 # Create kubernetes resources like alb-ingress-controller and deploy the 2048-game app
 KUBECONFIG=.kubeconfig kubectl apply -f kubernetes/
+
+# Wait for ingress to complete
+KUBECONFIG=.kubeconfig kubectl get ing 2048-ingress -n 2048-game
