112c57b5eeefb6010bd594ac9e7bc32ad9c9fc14
Megha Mehta <mmehta.ten@gmail.com>
2022-06-20 22:20:59 +0530

    [update] module for s3

---
+++ modules/s3/variables.tf
@@ -0,0 +1,14 @@
+variable "bucket_name" {
+  description = "Bucket name"
+  default     = "tf-bucket"
+}
+
+variable "vpc_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+# variable "aws_profile" {
+#   description = "AWS profile"
+#   default = "default"
+# }
