d3c37dd983851e703b081e825dff2bc7c2825a26
Megha Mehta <mmehta.ten@gmail.com>
2022-06-21 20:08:29 +0530

    [add] invoke lambda for authorizer

--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
 ###############################################################################
-# Providers
-###############################################################################
-provider "aws" {
-  region                  = var.vpc_region
-  shared_credentials_file = "~/.aws/credentials"
-  profile                 = "default"
-}
-
-###############################################################################
 # Base Network
 ###############################################################################
 
@@ -28,23 +19,34 @@ module "public_subnets" {
   cluster_name       = "eks_cluster_using_terraform"
 }
 
+# module "private_subnets" {
+#   source = "./modules/network/sn-private"
+
+#   az_count           = var.az_count
+#   availability_zones = var.availability_zones
+#   vpc_id             = module.vpc.id
+#   cluster_name       = "eks_cluster_using_terraform"
+# }
+
 terraform {
   backend "s3" {
-    bucket  = "terraform-backend-in-s3-for-eks-with-terraform"
+    bucket  = "s3-new-terraform-state-123"
     encrypt = true
     key     = "terraform.tfstate"
-    region  = "us-east-1" # hard-coded because vars are not allowed
+    region  = "us-east-1"
+    profile = "assignments"
   }
 }
 
-###############################################################################
-# Kubernetes
-###############################################################################
+# ###############################################################################
+# # Kubernetes
+# ###############################################################################
 
 module "eks" {
   source     = "./modules/compute/eks"
   region     = var.vpc_region
   subnet_ids = module.public_subnets.public_subnet_ids
+  # subnet_ids = module.private_subnets.private_subnet_ids
 }
 
 ###############################################################################
@@ -54,4 +56,9 @@ module "eks" {
 module "ingress" {
   source       = "./modules/network/ingress"
   cluster_name = module.eks.cluster_name
+}
+
+module "lambda" {
+  source       = "./modules/compute/lambda"
+  function_name = "lambda_authorizer"
 }
